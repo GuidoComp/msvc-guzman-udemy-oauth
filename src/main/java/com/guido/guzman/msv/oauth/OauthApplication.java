ea7ab819f0de29c0f5631207b5d44236d3d8f2e0
Guido Agustín Compagno <guidocompagno@gmail.com>
2025-06-12 19:16:32 -0300

    spring initializr first commit

---
+++ src/main/java/com/guido/guzman/msv/oauth/OauthApplication.java
@@ -0,0 +1,13 @@
+package com.guido.guzman.msv.oauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OauthApplication.class, args);
+	}
+
+}
